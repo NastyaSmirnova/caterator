d810ce12c78504ef5ab0d2cf50a26fb250690a10
nsmirnova <smirnovanastya1602@gmail.com>
2017-11-24 21:28:44 +0200

    Uploading app to gh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
 
 import { environment } from '../environments/environment';
 
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatButtonModule
+} from '@angular/material';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
